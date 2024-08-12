a0120d8e32a27beef1547b9c8404c845dd39ccb1
SamRaitan <samuelraitan2@gmail.com>
2024-08-12 16:27:51 +0300

    added avatar

--- Frontend/src/screens/profiles/visitedProfile/index.tsx
+++ Frontend/src/screens/profiles/visitedProfile/index.tsx
@@ -1,6 +1,7 @@
 import { Container, Group, Stack } from '@mantine/core';
 import { useParams } from 'react-router-dom';
 import { useFetchUserProfileQuery } from '../../../selectors/profiles';
+import ResponsiveAvatar from './avatar';
 
 type RouteParams = {
     username: string;
@@ -10,23 +11,21 @@ function VisitedProfile() {
     const { username } = useParams<RouteParams>();
     console.log(username);
 
-    const { data, isLoading, isError } = useFetchUserProfileQuery(username)
+    const { data, isLoading, isError } = useFetchUserProfileQuery(username);
     console.log(data);
 
-
     if (isLoading) return <p>Loading...</p>;
-    if (isError) return <p>Error loading recipe.</p>;
+    if (isError) return <p>Error loading profile.</p>;
 
     return (
         <Container>
             <Stack>
-                <Group justify="space-between">
-
+                <Group justify='space-between'>
+                    <ResponsiveAvatar src={data?.data.User.avatar} alt="User Avatar" />
                 </Group>
             </Stack>
         </Container>
-
     );
-};
+}
 
 export default VisitedProfile;
