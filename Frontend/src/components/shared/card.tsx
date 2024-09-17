c237363e6e7ac3253d7ebedc87546342c5f0da4f
SamRaitan <samuelraitan2@gmail.com>
2024-09-17 21:48:14 +0300

    regular modal and three dots button

--- Frontend/src/components/shared/card.tsx
+++ Frontend/src/components/shared/card.tsx
@@ -1,5 +1,6 @@
-import { Card, Image, Text, Group, Anchor, Rating, Badge } from '@mantine/core';
+import { Card, Image, Text, Group, Anchor, Rating, Badge, Menu, Modal, Button, Grid } from '@mantine/core';
 import { Recipe } from '../../types/recipe';
+import { useDisclosure } from '@mantine/hooks';
 
 type Props = {
     width: number;
@@ -8,53 +9,68 @@ type Props = {
 };
 
 function MainCard({ width, recipe, zoom }: Props) {
+    const [opened, { open, close }] = useDisclosure(false);
+
 
     return (
-        <Card
-            style={{
-                width: '320px',
-                height: '320px',
-                overflow: 'hidden',
-                transition: 'transform 0.2s ease',
-            }}
-            shadow="lg" padding="md" radius="lg" withBorder w={width}
-            onMouseEnter={(e) => zoom ? e.currentTarget.style.transform = 'scale(1.05)' : null}
-            onMouseLeave={(e) => zoom ? e.currentTarget.style.transform = 'scale(1)' : null}>
-            <Card.Section component="a" href={`/posts/${recipe._id}`} style={{ position: 'relative' }}>
-                <Image
-                    src={recipe.cloudinary_image}
-                    height={180}
-                    alt="recipe image"
-                    fit="cover"
-                />
-                <Badge
-                    color="teal"
-                    size="sm"
-                    style={{ position: 'absolute', top: 10, left: 10 }}>
-                    {recipe.cuisine}
-                </Badge>
-            </Card.Section>
+        <>
+            <Modal opened={opened} onClose={close} title="Authentication">
+                momo
+            </Modal>
 
-            <Group justify="space-between" mt="sm">
-                <Anchor href={`/userProfile/${recipe.uploader_un}`} size="xs" color="dimmed" target="_blank" underline="hover">
-                    {recipe.uploader_un}
-                </Anchor>
-                <Text size="xs" c="dimmed">{recipe.cookTime}</Text>
-            </Group>
+            <Card
+                style={{
+                    width: '320px',
+                    height: '320px',
+                    overflow: 'hidden',
+                    transition: 'transform 0.2s ease',
+                }}
+                shadow="lg" padding="md" radius="lg" withBorder w={width}
+                onMouseEnter={(e) => zoom ? e.currentTarget.style.transform = 'scale(1.05)' : null}
+                onMouseLeave={(e) => zoom ? e.currentTarget.style.transform = 'scale(1)' : null}>
+                <Card.Section component="a" href={`/posts/${recipe._id}`} style={{ position: 'relative' }}>
+                    <Image
+                        src={recipe.cloudinary_image}
+                        height={180}
+                        alt="recipe image"
+                        fit="cover"
+                    />
+                    <Badge
+                        color="teal"
+                        size="sm"
+                        style={{ position: 'absolute', top: 10, left: 10 }}>
+                        {recipe.cuisine}
+                    </Badge>
+                </Card.Section>
 
-            <Text lineClamp={1} fw={600} mt={4} size="md">
-                {recipe.title}
-            </Text>
+                <Group justify="space-between" mt="sm">
+                    <Anchor href={`/userProfile/${recipe.uploader_un}`} size="xs" color="dimmed" target="_blank" underline="hover">
+                        {recipe.uploader_un}
+                    </Anchor>
+                    <Text size="xs" c="dimmed">{recipe.cookTime}</Text>
+                </Group>
 
-            <Text lineClamp={2} size="xs" mt={2} color="dimmed">
-                {recipe.description}
-            </Text>
+                <Text lineClamp={1} fw={600} mt={4} size="md">
+                    {recipe.title}
+                </Text>
 
-            <Group justify="space-between" mt="md" align="center" gap="xs">
-                <Rating value={recipe.averageRating} readOnly size="xs" />
-                <Text size="xs" c="dimmed">({recipe.ratings.length})</Text>
-            </Group>
-        </Card>
+                <Text lineClamp={2} size="xs" mt={2} color="dimmed">
+                    {recipe.description}
+                </Text>
+
+                <Group justify="space-between" mt="md" align="center" gap="xs">
+                    <Grid>
+                        <Rating value={recipe.averageRating} readOnly size="xs" p={2} />
+                        <Text size="xs" c="dimmed">({recipe.ratings.length})</Text>
+                    </Grid>
+
+                    <Button size="xs" variant="subtle" c='teal' color='teal' radius="xl" onClick={open}>
+                        •••
+                    </Button>
+
+                </Group>
+            </Card>
+        </>
     );
 }
 
